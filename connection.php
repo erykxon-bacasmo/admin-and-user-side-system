d5e801eeb78e9b2090eca625dafcb1d150cb6718
Erykxon Bacasmo <114896787+erykxon@users.noreply.github.com>
2023-07-01 21:17:18 +0800

    I add some syntax here for other methods

--- connection.php
+++ connection.php
@@ -1,5 +1,22 @@
 <?php
 
+// Old method I used
+// function connection(){
+//     $host = "localhost";
+//     $user = "root";
+//     $pass = "";
+//     $db = "company_system";
+
+//     $conn = new mysqli("localhost", "root", "", "company_system");
+
+//     if($conn->connect_error){
+//         echo $conn->connect_error;
+//     } else {
+//         return $conn;
+//     }
+
+// }
+
 
 $conn = new mysqli("localhost", "root", "", "company_system");
 
